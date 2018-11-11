6ca314fd4b900491030233e2fe566b323f1cba37
Xavier Bawerk <dpllgr@hotmail.com>
2018-11-11 13:49:21 -0600

    Agregando ejercicios capitulo 1

---
+++ pr2_29.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+int main()
+{
+    int numero=0;
+    cout << "Numero" << "   "<< "Cuadrado" << "   " << "Cubo" << endl;
+    cout << numero << "           "<< numero*numero << "        " << numero*numero*numero << endl;
+    numero= numero +1;
+    cout << numero << "           "<< numero*numero << "        " << numero*numero*numero << endl;
+    numero= numero +1;
+    cout << numero << "           "<< numero*numero << "        " << numero*numero*numero << endl;
+    numero= numero +1;
+    cout << numero << "           "<< numero*numero << "        " << numero*numero*numero << endl;
+    numero= numero +1;
+    cout << numero << "           "<< numero*numero << "        " << numero*numero*numero << endl;
+    numero= numero +1;
+    cout << numero << "           "<< numero*numero << "        " << numero*numero*numero << endl;
+    numero= numero +1;
+    cout << numero << "           "<< numero*numero << "        " << numero*numero*numero << endl;
+    numero= numero +1;
+    cout << numero << "           "<< numero*numero << "        " << numero*numero*numero << endl;
+    numero= numero +1;
+    cout << numero << "           "<< numero*numero << "        " << numero*numero*numero << endl;
+    numero= numero +1;
+    cout << numero << "           "<< numero*numero << "        " << numero*numero*numero << endl;
+    numero= numero +1;
+    cout << numero << "         "<< numero*numero << "        " << numero*numero*numero << endl;
+    numero= numero +1;
+        return 0;
+    
+
+}
